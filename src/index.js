6e0ae5af2b67ce1654da94b3761a97b9fe6df8e0
LindaSz0112 <lindaszarvas@gmail.com>
2023-02-19 14:45:16 +0100

    Modified and Added search engine to code

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ if (currentMinutes < 10) {
 todayDate.innerHTML = `${currentDay}.${currentMonth}.${currentYear}.`;
 nowTime.innerHTML = `${currentHour}:${currentMinutes}`;
 
-function weatherInformation(response) {
+/*function weatherInformation(response) {
   let temperatureValue = document.querySelector("#temp-value");
   temperatureValue.innerHTML = Math.round(response.data.main.temp);
   let cityName = document.querySelector("#city");
@@ -35,13 +35,52 @@ function weatherInformation(response) {
   );
   let description = document.querySelector("#short-description");
   description.innerHTML = response.data.weather[0].description;
+}*/
+
+function cityWeather(city) {
+  let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeatherDeatails);
 }
 
-let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  cityWeather(city);
+}
+
+function showWeatherDeatails(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#temp-value").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#wind").innerHTML = `${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `${response.data.main.humidity}%`;
+  document.querySelector("#temp-max").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#temp-min").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  let description = document.querySelector("#short-description");
+  description.innerHTML = response.data.weather[0].description;
+}
+
+let enterCity = document.querySelector("#city-search");
+enterCity.addEventListener("click", showCity);
+
+/*let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=prague&units=metric&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(weatherInformation);
+console.log(apiUrl);*/
+
+/*axios.get(apiUrl).then(weatherInformation);*/
+cityWeather("Prague");
 
 /*function cityWeather(city) {
   let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
